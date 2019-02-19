cde4260e1915f9087099b051dea3348eeb036ee7
Matthew Setter <matthew@matthewsetter.com>
2019-02-19 10:31:05 +0100

    Rename administration_manual to admin_manual
    This change:
    
    - Renames the administration_manual directory to admin_manual
    - Updates all xrefs
    - Updates the (PDF) book configuration files
    - Updates the Makefile target for building the admin manual

--- modules/admin_manual/examples/installation/lets_encrypt/your-domain-name.sh
+++ modules/admin_manual/examples/installation/lets_encrypt/your-domain-name.sh
@@ -4,8 +4,8 @@
 LE_PATH="/usr/bin"
 LE_CB="certbot"
 
-# Assumes that example.com www.example.com and subomain.example.com are the domains 
-# that you want a certificate for
-export DOMAINS="-d example.com -d www.example.com -d subdomain.example.com"
+# Assumes that mydom.tld www.mydom.tld and sub.mydom.tld are the domains that you want a certificate for
+export DOMAINS="-d mydom.tld -d www.mydom.tld -d sub.mydom.tld"
 
 "$LE_PATH/$LE_CB" certonly --config /etc/letsencrypt/cli.ini "$DOMAINS" # --dry-run
+
