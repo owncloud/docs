494ca9946a92735b2b3af31ed7f149cfc2bd7150
Matthew Setter <matthew@matthewsetter.com>
2019-03-29 07:12:04 +0100

    Updated the backport script
    On reviewing the script, I felt that there were several places where it
    could be further improved. Specifically, I:
    
    - Refactored the checks for required commands into a function
    - Refactored the printing of the commit list into a function
    - Quoted more variables to avoid word-splitting and globbing
    - Optimised the rate limit reset and remaining retrieval process
    - Refactored the time remaining process to work on macOS (Darwin)

--- modules/developer_manual/examples/scripts/backport.sh
+++ modules/developer_manual/examples/scripts/backport.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-scriptDependencies=( curl git grep jq perl tail )
+set -o pipefail
+set -o nounset
 
-function checkDependencies() {
-    for dependency in "${scriptDependencies[@]}"
-    do
-        if ! [ -x "$(command -v ${dependency})" ]; then
-            echo "Error: ${dependency} is not installed." >&2
-            echo "Please install package \"${dependency}\" before using this script"
-            exit 1
+function printCommitList() {
+    local commitList="$1"
+
+    # TODO: Might be able to refactor this to be somewhat simpler. Maybe.
+    commitCount=$(echo "$commitList" | grep '' | wc -l | tr -d ' ')
+    if [[ $commitCount -ne 0 ]]; then
+        if [[ $commitCount -eq 1 ]]; then
+            echo "$commitCount commit being cherry picked:"
+        else
+            echo "$commitCount commits being cherry picked:"
         fi
-    done
+
+        for i in $commitList; do 
+            printf "* %s\n" $i 
+        done
+    fi
 }
 
-checkDependencies
+function checkBinaryIsAvailable() {
+    local binary="$1"
+    if ! [ -x "$(command -v ${binary})" ]; then
+        echo "Error: ${binary} is not installed." >&2
+        echo "Please install package "${binary}" before using this script"
+        exit 1
+    fi
+}
+
+requiredBinaries=('jq' 'curl' 'git' 'grep' 'perl')
+for i in "${requiredBinaries[@]}"; do 
+    checkBinaryIsAvailable "$i"
+done
 
 if [ "$#" -lt 2 ]; then
     echo "Illegal number of parameters"
@@ -22,106 +42,114 @@ if [ "$#" -lt 2 ]; then
     exit
 fi
 
-commit="$1"
-targetBranch="$2"
-baseBranch="$(git rev-parse --abbrev-ref HEAD)"
+commit=$1
+targetBranch=$2
+baseBranch=$(git rev-parse --abbrev-ref HEAD)
+is_merged="$(git branch --contains "$commit" | grep -oP '(?<=\*).*')"
 
-is_merged="$(git branch --contains $1 | grep -oP '(?<=\*).*')"
 if [ -z "$is_merged" ]; then
-    echo "${commit} has not been merged or branch ${baseBranch} is not pulled/rebased. Exiting"
+    echo "$commit has not been merged or branch $baseBranch is not pulled/rebased. Exiting"
     echo
     exit
 fi
 
-# Get the PR number from commit
-pullId="$(git log $1^! --oneline 2>/dev/null | tail -n 3 | grep -oP '(?<=#)[0-9]*')"
+# get the PR number from commit
+pullId=$(git log $1^! --oneline 2>/dev/null | tail -n 3 | grep -oP '(?<=#)[0-9]*')
 
-# Get the repository from commit
-repository="$(git config --get remote.origin.url 2>/dev/null | perl -lne 'print $1 if /(?:(?:https?:\/\/github.com\/)|:)(.*?).git/')"
+# get the repository from commit
+repository=$(git config --get remote.origin.url 2>/dev/null | perl -lne 'print $1 if /(?:(?:https?:\/\/github.com\/)|:)(.*?).git/')
 
-# Get the list of commits in the PR without any merge commit.
+# get the list of commits in PR without any merge commit
 # $1^ means the first parent of the merge commit (that is passed in as $1).
 # Because $1 is a "magically-generated" merge commit, it happily "jumps back"
 # to the point on the main branch just before where the PR was merged.
 # And so the commits from that point are exactly the list of individual
 # commits in the original PR.
 # --no-merges leaves out the merge commit itself, and we get just what we want
-commitList="$(git log --no-merges --reverse --format=format:%h $1^..$1)"
+commitList=$(git log --no-merges --reverse --format=format:%h $1^..$1)
 
-#. <( curl -i https://api.github.com/users/octocat 2>&1 | grep -P '^X-RateLimit-(Reset|Remaining)' | sed 's/X-RateLimit-/rateLimit/g; s/: /=/g;' )
+rateLimitStatus="$( curl -i https://api.github.com/users/octocat 2>&1 | grep -P '^X-RateLimit-(Reset|Remaining)' )"
 
-# Get the request reset time window from github in epoch
-rateLimitReset="$( echo $rateLimitInfo | grep -m1 'X-RateLimit-Reset:' | grep -o '[[:digit:]]*')"
+# get the request reset time window from github in epoch
+rateLimitReset="$( echo "$rateLimitStatus" | grep -m1 'X-RateLimit-Reset:' | grep -o '[[:digit:]]*' )"
 
-# Get the remaining requests in window from GitHub
-rateLimitRemaining="$( echo $rateLimitInfo | grep -m1 'X-RateLimit-Remaining:' | grep -o '[[:digit:]]*')"
+# get the remaining requests in window from github
+rateLimitRemaining="$( echo "$rateLimitStatus" | grep -m1 'X-RateLimit-Remaining:' | grep -o '[[:digit:]]*' )"
 
 # time remaining in epoch
-now="$(date +%s)"
-((remaining=rateLimitReset-now))
+now=$(date +%s)
+remaining=$((rateLimitReset - now))
+operatingSystem=$( uname -s | tr '[:upper:]' '[:lower:]' )
 
 # Time remaining in HMS
-remaining="$(date -u -d @${remaining} +%H:%M:%S)"
+case $operatingSystem in 
+    darwin) 
+        remaining=$(date -r $remaining +%H:%M:%S)
+        ;;
+    linux | *)
+        remaining=$(date -u -d @$remaining +%H:%M:%S)
+        ;;
+esac
 
-# Check if there are commits to cherry pick and list them in case
+# check if there are commits to cherry pick and list them in case
 if [[ -z "$commitList" ]]; then
   echo "There are no commits to cherry pick. Exiting"
   exit
 else
-  lineCount="$(echo "$commitList" | grep '' | wc -l)"
-  echo "${lineCount} commits being cherry picked:"
-  echo
-  echo "${commitList}"
+  printCommitList "$commitList"
 fi
 
-if (( "${rateLimitRemaining}" <= 0 )); then
-  # Do not continue if there are no remaining github requests available
-  echo "You do not have enough GitHub requests available to backport the PR."
-  echo "The current rate limit window resets in ${remaining}."
-  exit
+if [ $rateLimitRemaining -le 0 ]; then
+  # do not continue if there are no remaining github requests available
+  echo "You do not have enough github requests available to backport"
+  echo "The current rate limit window resets in $remaining"
+  exit 
 else
-  # Get the PR title, as this is the only automated valid way to get the title.
-  pullTitle="$(curl https://api.github.com/repos/${repository}/pulls/${pullId} 2>/dev/null | jq '.title' | sed 's/^.//' | sed 's/.$//')"
+  # get the PR title, this is the only automated valid way to get the title 
+  pullTitle=$(curl https://api.github.com/repos/$repository/pulls/$pullId 2>/dev/null | jq '.title' | sed 's/^.//' | sed 's/.$//')
 fi
 
-# Build names used
-targetCommit="${targetBranch}-${commit}-${pullId}"
-message="[${targetBranch}] [PR ${pullId}] ${pullTitle}"
+# build names used
+targetCommit="$targetBranch-$commit-$pullId"
+message="[$targetBranch] [PR $pullId] $pullTitle"
 
 echo
 echo "Info:"
-echo "You have ${rateLimitRemaining} backport requests remaining in the current GitHub rate limit window."
-echo "The current rate limit window resets in ${remaining}."
+echo "You have $rateLimitRemaining backport requests remaining in the current github rate limit window"
+echo "The current rate limit window resets in $remaining"
 echo
-echo "Backporting commit ${commit} to ${targetBranch} with the following text:"
-echo "${message}"
+echo "Backporting commit $commit to $targetBranch with the following text:"
+echo "$message"
 echo
 
 set -e
 
 git fetch -p --quiet
-git checkout "${targetBranch}"
+git checkout "$targetBranch"
 git pull --rebase --quiet
-git checkout -b "${targetCommit}"
+git checkout -b "$targetCommit"
 
 echo
 
-# Cherry pick all commits from the commit list.
+# cherry pick all commits from commitList
 lC=1
 echo "$commitList" | while IFS= read -r line; do
   echo "Cherry picking commit $lC: $line"
-  # If you do not want to use a default conflict resolution to take theirs
+  # if you do not want to use a default conflict resolution to take theirs
   # (help fix missing cherry picked commits or file renames)
-  #git cherry-pick $line > /dev/null
-  git cherry-pick -Xtheirs $line > /dev/null
+  #git cherry-pick $line > /dev/null 
+  git cherry-pick -Xtheirs $line > /dev/null 
   lC=$(( $lC + 1 ))
 done
 echo
 
-git commit --quiet --amend -m "${message}" -m "Backport of PR #${pullId}"
+git commit --quiet --amend -m "$message" -m "Backport of PR #$pullId"
 
-echo "Pushing: ${message}"
+echo "Pushing: $message"
 echo
 
-git push --quiet -u origin "${targetCommit}"
-git checkout --quiet "${baseBranch}"
+git push --quiet -u origin "$targetCommit"
+git checkout --quiet "$baseBranch"
+
+
+
