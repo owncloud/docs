b5318ee44fa62779680c506d0a36aca3afdc561c
Matthew Setter <settermjd@users.noreply.github.com>
2019-06-25 14:06:48 +0200

    Update the Ubuntu 18.04 installation example (#1302)
    This change makes a number of changes with the view to making the guide
    less terse, yet keeping it short, sweet, and to the point. It:
    
    - Reworked the header structure
    - Fixed spelling and grammatical errors
    - Updated headers to follow the style guide
    - Updated code examples to be that much simpler to read and to use
    syntax highlighting, where possible
    - Extracted some code samples out into example files, making them easier
    to maintain and test/lint
    - Updated code examples to use sudo, which is the convention on Ubuntu
    - Linked to relevant information, both internal and external, to help
    users of varying skill and knowledge levels

---
+++ modules/admin_manual/examples/installation/ubuntu/18.04/memcache-configuration.php
@@ -0,0 +1,8 @@
+<?php
+// ...remaining configuration
+'memcache.local' => '\OC\Memcache\APCu',
+'memcache.locking' => '\OC\Memcache\Redis',
+'redis' => [
+    'host' => 'localhost',
+    'port' => 6379,
+],
