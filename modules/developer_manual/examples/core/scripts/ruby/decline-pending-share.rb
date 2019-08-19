1ee5ec46b41a7c4febf86def1adc04864aebd00b
Matthew Setter <settermjd@users.noreply.github.com>
2019-08-19 10:23:20 +0200

    [10.1] [PR 1570] Update the OCS Share API Get Shares From Specific File or Folder docs (#1663)
    Backport of PR #1570

---
+++ modules/developer_manual/examples/core/scripts/ruby/decline-pending-share.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'uri'
+
+base_uri = '{oc-examples-server-url}/ocs/v1.php/apps/files_sharing/api/v1/'
+uri = URI("#{base_uri}/shares/pending/<share_id>")
+
+Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+  req = Net::HTTP::Delete.new uri
+  req.basic_auth 'your.username', 'your.password'
+  res = http.request req
+
+  puts res.body
+end
+
