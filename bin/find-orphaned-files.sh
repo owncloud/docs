3aaba321613474222105ffd249b3db17c8ffac37
mmattel <martin.mattel@diemattels.at>
2019-09-15 12:36:51 +0200

    improvement

--- bin/find-orphaned-files.sh
+++ bin/find-orphaned-files.sh
@@ -8,6 +8,7 @@
 MANUALS=( admin developer user )
 
 echo "Checking for orphaned example files."
+echo "Please run this scrip from your docs root"
 echo
 
 for manual in "${MANUALS[@]}"; do
@@ -15,10 +16,12 @@ for manual in "${MANUALS[@]}"; do
 
     echo "Checking the ${manual} manual"
     find "./modules/${manual}_manual/examples/" \
-        \( -path "**/.DS_Store" -o -path "**/vendor" -o -path "**/.idea" -o -path "**/.gitkeep" \) -prune -o \
+        \( -path "**/.DS_Store" -o -path "**/vendor" -o -path "**/.gitkeep" \) -prune -o \
         -type f \
         -exec bash -c 'grep -rnq -E "^include.*$(basename $0 | sed "s/\./\\./")" ./modules/$1_manual/pages || echo "Detected orphan file: $0"' {} "${manual}" \;
     echo
 done
 
 echo "Check completed."
+echo "Please consider that files may have been added intentionally but have currently not been included in the documeantation."
+echo "You may want to check the orphan history via 'git log --full-history -- full_path_file_from_above'."
