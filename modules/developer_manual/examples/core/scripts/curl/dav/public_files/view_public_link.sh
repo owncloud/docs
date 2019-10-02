06608047e373b919f699052b3d6c7345376be19d
Matthew Setter <matthew.setter@gmail.com>
2019-10-02 15:13:56 +0200

    Document the public-files WebDAV API endpoint
    This relates to owncloud/core/pull/35932 and #1871.

---
+++ modules/developer_manual/examples/core/scripts/curl/dav/public_files/view_public_link.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+##
+## Variable Declaration
+##
+USERNAME={oc-examples-username}
+PASSWORD={oc-examples-password}
+API_PATH="/remote.php/dav/public-files/<share_token>"
+SERVER_URI="{oc-examples-server-url}"
+REQUEST_BODY=$(cat <<EOF                                                                
+<?xml version="1.0"?>                                 
+<d:propfind xmlns:d="DAV:" xmlns:oc="http://owncloud.org/ns">
+    <d:prop>   
+        <oc:public-link-item-type />
+        <oc:public-link-item-permission />
+        <oc:public-link-item-expiration />
+        <oc:public-link-item-share-datetime />
+        <oc:public-link-item-owner />
+    </d:prop>
+</d:propfind>
+EOF
+)
+
+curl "'$SERVER_URI/$API_PATH" \
+    -H 'Content-Type: application/xml; charset=UTF-8' \
+    -H 'Depth: 1' \
+    -X PROPFIND \
+    --data-binary "${REQUEST_BODY}" \
+    --user "${USERNAME}:${PASSWORD}" | xml_pp
---
+++ modules/developer_manual/examples/core/scripts/php/dav/public_files/view_public_link.php
@@ -0,0 +1,27 @@
+<?php
+
+use GuzzleHttp\Client;
+
+require_once('vendor/autoload.php');
+
+// Configure the basic client
+$client = new Client([
+    'base_uri' => '{oc-examples-server-url}/remote.php/dav/',
+]);
+
+$share_token = '<share_token>';
+
+try {
+    $response = $client->request('PROPFIND', "public-files/${share_token}", [
+        'auth' => [
+            '{oc-examples-username}',
+            '{oc-examples-password}'
+        ],
+        'headers' => [
+            'Content-Type'=> 'text/xml',
+        ],
+    ]);
+    print $response->getBody()->getContents();
+} catch (\GuzzleHttp\Exception\ClientException $e) {
+    print $e->getMessage();
+}
