3aaac1d03c585615c4b6477ad37bc87bf508f1f4
Matthew Setter <settermjd@users.noreply.github.com>
2019-10-02 12:06:37 +0200

    [10.3] [PR 1874] Document the attributes field of the create-share API (#1878)
    Backport of PR #1874

--- modules/developer_manual/examples/core/scripts/curl/create-share.sh
+++ modules/developer_manual/examples/core/scripts/curl/create-share.sh
@@ -4,8 +4,17 @@
 ## Variable Declaration
 ##
 SERVER_URI=https://your.owncloud.install.com/owncloud
-API_PATH=ocs/v1.php/apps/files_sharing/api/v1
+API_PATH=ocs/v1.php/apps/files_sharing/api/v1/shares
 
-curl --user your.username:your.password "$SERVER_URI/$API_PATH/shares" \
-     --data 'path=/Photos/Paris.jpg&shareType=3&permissions=1&name=paris%20photo'
+# Create a public link share with read permissions, named "paris photo"
+curl --user your.username:your.password "$SERVER_URI/$API_PATH" \
+     --data 'path=/Photos/Paris.jpg&shareType=3&permissions=3&name=paris%20photo'
 
+# Create a user share with read permissions, named "welcome.txt" that has read
+# and share permissions set.
+curl --silent --user "{oc-examples-username}:{oc-examples-password}" \
+     "$SERVER_URI/$API_PATH/" \
+     --data 'path=/welcome.txt' \
+     --data 'shareType=3' \
+     --data 'name=welcome.txt' \
+     --data 'attributes[0][scope]=ownCloud&attributes[0][key]=read&attributes[0][value]=true'
