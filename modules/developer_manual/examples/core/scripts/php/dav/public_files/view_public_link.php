06404d886cd1f79285771a96e55c862d65f5fda7
Matthew Setter <settermjd@users.noreply.github.com>
2019-10-10 09:00:50 +0200

    [10.3] [PR 1879] Document the public-files WebDAV API endpoint
    Backport of PR #1879

---
+++ modules/developer_manual/examples/core/scripts/php/dav/public_files/view_public_link.php
@@ -0,0 +1,23 @@
+<?php
+
+use GuzzleHttp\Client;
+
+require_once('vendor/autoload.php');
+
+// Configure the basic client
+$client = new Client([
+    'base_uri' => '{oc-examples-server-url}/remote.php/dav/',
+]);
+
+$share_token = '<share_token>';
+
+try {
+    $response = $client->request('PROPFIND', "public-files/${share_token}", [
+        'headers' => [
+            'Content-Type'=> 'text/xml',
+        ],
+    ]);
+    print $response->getBody()->getContents();
+} catch (\GuzzleHttp\Exception\ClientException $e) {
+    print $e->getMessage();
+}
