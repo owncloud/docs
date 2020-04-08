ae1f2716f7ee40bf1f8280e7bd4b9aa4747227bb
Matthew Setter <matthew.setter@gmail.com>
2020-04-08 10:59:29 +0200

    Include the base OIDC configurations

---
+++ modules/admin_manual/examples/configuration/user/oidc/config-with-service-discovery.php
@@ -0,0 +1,8 @@
+<?php
+
+'openid-connect' => [
+  'client-id' => '',
+  'client-secret' => '',
+  'loginButtonName' => '',
+  'provider-url' => '',
+],
---
+++ modules/admin_manual/examples/configuration/user/oidc/config-without-service-discovery.php
@@ -0,0 +1,21 @@
+<?php
+
+'openid-connect' => [
+  'client-id' => '',
+  'client-secret' => '',
+  'loginButtonName' => 'OpenId Connect',
+  'autoRedirectOnLoginPage' => false,
+  'mode' => 'userid',
+  'provider-url' => '',
+  'search-attribute' => 'sub',
+  'use-token-introspection-endpoint' => true,
+  'provider-params' => [
+    'authorization_endpoint' => '',
+    'end_session_endpoint' => '',
+    'jwks_uri' => '',
+    'registration_endpoint' => '',
+    'token_endpoint' => '',
+    'token_endpoint_auth_methods_supported' => '',
+    'userinfo_endpoint' => ''
+  ],
+],
