ff29126df4403920dd04ef70da5cfc5b1003d139
Matthew Setter <matthew@matthewsetter.com>
2020-04-20 16:20:27 +0200

    Update finalise-install script
    The intent here is to simplify some of the script, so that it's more
    repeatable, make the permission settings more consistent, make the
    script that much more verbose and reliable.

--- modules/admin_manual/examples/installation/ubuntu/18.04/finalise-install.sh
+++ modules/admin_manual/examples/installation/ubuntu/18.04/finalise-install.sh
@@ -1,44 +1,62 @@
 FILE="/usr/local/bin/ocpermissions"
 
 /bin/cat <<EOM >$FILE
-#!/bin/bash 
+#!/bin/bash
 
-ocpath="{install-directory}" 
-datadir="${ocpath}/data" 
-htuser="{webserver-user}" 
-htgroup="{webserver-group}" 
-rootuser="root" 
+ocpath="/var/www/owncloud"
+datadir="${ocpath}/data"
+htuser="www-data"
+htgroup="www-data"
 
-printf "Creating any missing directories" 
-sudo -u "${htuser}" mkdir -p "${ocpath}/assets" 
-sudo -u "${htuser}" mkdir -p "${ocpath}/updater" 
-sudo -u "${htuser}" mkdir -p "${datadir}" 
+printf "Running ${0}"
+echo
 
-printf "Update file and directory permissions" 
-sudo find "${ocpath}/" -type f -print0 | xargs -0 chmod 0640 
-sudo find "${ocpath}/" -type d -print0 | xargs -0 chmod 0750 
+echo "Creating any missing directories."
+sudo -u "${htuser}" mkdir -p "${ocpath}/assets"
+sudo -u "${htuser}" mkdir -p "${ocpath}/updater"
+sudo -u "${htuser}" mkdir -p "${datadir}"
 
-printf "Set web server user and group as ownCloud directory user and group" 
-sudo chown -R "${rootuser}:${htgroup}" "${ocpath}/" 
-sudo chown -R "${htuser}:${htgroup}" "${ocpath}/apps/" 
-sudo chown -R "${htuser}:${htgroup}" "${ocpath}/apps-external/" 
-sudo chown -R "${htuser}:${htgroup}" "${ocpath}/assets/" 
-sudo chown -R "${htuser}:${htgroup}" "${ocpath}/config/" 
-sudo chown -R "${htuser}:${htgroup}" "${datadir}" 
-sudo chown -R "${htuser}:${htgroup}" "${ocpath}/updater/" 
+echo "Updating file and directory permissions."
+sudo find "${ocpath}/" -type f -print0 | xargs -0 chmod 0644
+sudo find "${ocpath}/" -type d -print0 | xargs -0 chmod 0754
+
+# Directories to update permissions on
+directories=(
+  "${ocpath}/" 
+  "${ocpath}/apps/" 
+  "${ocpath}/apps-external/" 
+  "${ocpath}/assets/" 
+  "${ocpath}/config/" 
+  "${datadir}/" 
+  "${ocpath}/updater/" 
+)
+set_dir_ownership_and_permissions() {
+  if [ -e "$1" ] && [ -d "$1" ]; then
+    path="${1}"
+    printf "Setting owner of \"%s\" to \"%s\" and group to \"%s\"\n" "${path}" "${htuser}" "${htgroup}"
+    sudo chown -R "${htuser}:${htgroup}" "${path}"
+  fi;
+}
+
+for directory in "${directories[@]}"
+do
+  set_dir_ownership_and_permissions "${directory}"
+done;
 sudo chmod +x "${ocpath}/occ"
 
-printf "Set web server user and group as .htaccess user and group" 
-if [ -f "${ocpath}/.htaccess" ]; then  
-  sudo chmod 0644 "${ocpath}/.htaccess"  
-  sudo chown "${rootuser}:${htgroup}" "${ocpath}/.htaccess" 
-fi 
+echo "Setting web server user and group as .htaccess owner and group."
+if [ -f "${ocpath}/.htaccess" ]; then
+  sudo chmod 0664 "${ocpath}/.htaccess"
+  sudo chown "${htuser}:${htgroup}" "${ocpath}/.htaccess"
+fi
 
-if [ -f "${datadir}/.htaccess" ]; then  
-  sudo chmod 0644 "${datadir}/.htaccess"  
-  sudo chown "${rootuser}:${htgroup}" "${datadir}/.htaccess"
-fi 
+if [ -f "${datadir}/.htaccess" ]; then
+  sudo chmod 0664 "${datadir}/.htaccess"
+  sudo chown "${htuser}:${htgroup}" "${datadir}/.htaccess"
+fi
 
+echo "Exiting"
+echo
 EOM
 
 # Make the script executable
