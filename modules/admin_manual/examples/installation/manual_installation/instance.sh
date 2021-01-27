f0c7cf2c876f42c24f151526071e16c37b69f5c3
Jürgen Weigert <jnweiger@gmail.com>
2021-01-27 16:46:09 +0100

    [10.6] [PR 3076] Update instance.sh
    Backport of PR #3076

--- modules/admin_manual/examples/installation/manual_installation/instance.sh
+++ modules/admin_manual/examples/installation/manual_installation/instance.sh
@@ -27,14 +27,7 @@ if [ "$(id -u)" != 0 ]; then
 fi
 
 # Resolve the absolute path this script is located and expects the called script to be there too
-# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 $DIR/owncloud_prep.sh "$ocname" "$ocroot" "$linkroot" "$htuser" "$htgroup" "$rootuser"
 
